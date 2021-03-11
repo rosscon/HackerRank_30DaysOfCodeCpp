6008b48cec8ce76a6cab601f44a64798274dc186
Ross Conroy <9072796+rosscon@users.noreply.github.com>
2021-03-11 07:58:12 +0000

    day 20 complete

---
+++ day_20_sorting/day20.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int a_i = 0; a_i < n; a_i++)
+    {
+        cin >> a[a_i];
+    }
+    
+    int totalSwaps = 0;
+    for (int i = 0; i < n; i++) {
+        // Track number of elements swapped during a single array traversal
+        int numberOfSwaps = 0;
+        
+        for (int j = 0; j < n - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                numberOfSwaps++;
+            }
+        }
+
+        totalSwaps += numberOfSwaps;
+        
+        // If no elements were swapped during a traversal, array is sorted
+        if (numberOfSwaps == 0) {
+            break;
+        }
+    }
+
+    cout << "Array is sorted in " << totalSwaps << " swaps." << endl;
+    cout << "First Element: " << a.front() << endl;
+    cout << "Last Element: " << a.back() << endl;
+
+
+    return 0;
+}
